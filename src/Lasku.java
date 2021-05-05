5dfced13bac7c044a60c8d5525bfc771e4d2202a
Santeri Mertakorpi <santemer@uef.fi>
2021-05-05 20:21:40 +0300

    lahetaLasku()

--- src/Lasku.java
+++ src/Lasku.java
@@ -4,6 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 
@@ -225,5 +233,41 @@ public class Lasku {
         } catch (IOException e){
             throw e;
         }        
+    }
+
+    public void lahetaLasku(){
+        String username = "mertakorpisanteri@gmail.com";
+        String password = "StpM2499";
+
+        Properties props = new Properties();
+        props.put("mail.stmp.starttls.enable", "true");
+        props.put("mail.smpt.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+        new javax.mail.Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(username, password);
+            }
+
+        });
+
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("mertakorpisanteri@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(getSposti()));
+            message.setSubject("VuokraToimistot Oy lasku");
+            message.setText("LASKU\n" 
+            + getEtunimi() + " " + getSukunimi() + "\n"
+            + getKatu() + " " + getPostinro() + "\n"
+            + getToimipaikka() + "\n"
+            + getSposti() + "\n"
+            + getSumma() + "\n"
+            + getErapaiva());
+            Transport.send(message);
+        }catch (MessagingException e){
+            throw new RuntimeException(e);
+        }
     }
 }
