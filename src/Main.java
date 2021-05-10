fd563bb49b9b84d3e3fd391647ccf8788ad3a7f6
Santeri Mertakorpi <santemer@uef.fi>
2021-05-10 20:21:01 +0300

    ...

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/src/LaskuGUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Lasku.fxml"));
         primaryStage.setTitle("VuokraToimistot OY");
         primaryStage.setScene(new Scene(root, 1000,600));
         primaryStage.show();
